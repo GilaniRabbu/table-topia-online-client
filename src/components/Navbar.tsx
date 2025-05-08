47dac78c436b356d095ffc5fd4b38d2405ba62df
GilaniRabbu <toufiqjilanirabbu7@gmail.com>
2025-05-08 23:42:44 +0600

    updated code

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,7 +1,6 @@
-
-import React, { useState } from 'react';
-import { Link } from 'react-router-dom';
-import { Menu, X } from 'lucide-react';
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
+import { Menu, X } from "lucide-react";
 
 const Navbar = () => {
   const [isOpen, setIsOpen] = useState(false);
@@ -17,20 +16,26 @@ const Navbar = () => {
 
         {/* Desktop Navigation */}
         <div className="hidden md:flex items-center space-x-6">
-          <Link to="/" className="text-gray-700 hover:text-restaurant-primary transition duration-300">
+          <Link
+            to="/"
+            className="text-gray-700 hover:text-restaurant-primary transition duration-300"
+          >
             Home
           </Link>
-          <Link to="/restaurants" className="text-gray-700 hover:text-restaurant-primary transition duration-300">
+          <Link
+            to="/restaurants"
+            className="text-gray-700 hover:text-restaurant-primary transition duration-300"
+          >
             Restaurants
           </Link>
-          <Link to="#" className="btn-primary">
+          <Link to="/restaurants" className="btn-primary">
             Make a Reservation
           </Link>
         </div>
 
         {/* Mobile Menu Button */}
         <div className="md:hidden">
-          <button 
+          <button
             onClick={() => setIsOpen(!isOpen)}
             className="text-gray-700 hover:text-restaurant-primary focus:outline-none"
           >
@@ -43,22 +48,22 @@ const Navbar = () => {
       {isOpen && (
         <div className="md:hidden bg-white w-full shadow-lg animate-fade-in">
           <div className="container mx-auto py-4 flex flex-col space-y-4">
-            <Link 
-              to="/" 
+            <Link
+              to="/"
               className="text-gray-700 hover:text-restaurant-primary transition duration-300 px-4 py-2"
               onClick={() => setIsOpen(false)}
             >
               Home
             </Link>
-            <Link 
-              to="/restaurants" 
+            <Link
+              to="/restaurants"
               className="text-gray-700 hover:text-restaurant-primary transition duration-300 px-4 py-2"
               onClick={() => setIsOpen(false)}
             >
               Restaurants
             </Link>
-            <Link 
-              to="#" 
+            <Link
+              to="#"
               className="btn-primary mx-4 text-center"
               onClick={() => setIsOpen(false)}
             >
